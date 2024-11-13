e83251d19cb646b4bc52a68668a2ce2d5d602712
MarcinOstaszewski <MarcinOstaszewski@gmail.com>
2024-11-13 11:01:06 +0100

    fix font size too big on printed page;

--- src/gulpfile.mjs
+++ src/gulpfile.mjs
@@ -14,8 +14,6 @@ const config = {
   destDir: '../dist',
   cssFiles: ['./styles/normalize.css', './styles/styles.css'],
   jsFiles: './scripts/index.js',
-  destCSS: '../dist',
-  destJS: '../dist',
   replace: {
     from: /assets/g,
     to: 'https://www.fronty-meblowe.pl/formularz/wp-content/uploads/2024/11'
@@ -31,7 +29,7 @@ gulp.task('styles', () => {
     .pipe(concat('styles.css'))
     .pipe(cleanCSS())
     .pipe(rename({ suffix: '.min' }))
-    .pipe(dest(config.destCSS));
+    .pipe(dest(config.destDir));
 });
 
 gulp.task('scripts', () => {
@@ -39,16 +37,16 @@ gulp.task('scripts', () => {
     .pipe(concat('scripts.js'))
     // .pipe(uglify())
     .pipe(rename({ suffix: '.min' }))
-    .pipe(dest(config.destJS));
+    .pipe(dest(config.destDir));
 });
 
 gulp.task('inject-inline-css-js-remove-start-end', () => {
-  const cssContent = fs.readFileSync(`${config.destCSS}/styles.min.css`, 'utf8');
-  const jsContent = fs.readFileSync(`${config.destJS}/scripts.min.js`, 'utf8');
+  const cssContent = fs.readFileSync(`${config.destDir}/styles.min.css`, 'utf8');
+  const jsContent = fs.readFileSync(`${config.destDir}/scripts.min.js`, 'utf8');
   return src('./fronty_proste.html')
     // .pipe(replace(config.replace.from, config.replace.to)) // paths to images
-    .pipe(injectString.replace(config.inject.css, `<style>${cssContent}</style>`)) // styles inlined
-    .pipe(injectString.replace(config.inject.js, `<script>${jsContent}</script>`)) // scripts inlined
+    .pipe(injectString.replace(config.inject.css, `<style> ${cssContent} </style>`)) // styles inlined
+    .pipe(injectString.replace(config.inject.js, `<script> ${jsContent} </script>`)) // scripts inlined
     .pipe(gulpRemoveHtml())
     .pipe(rename('fronty_proste__kod_WordPressa.html'))
     .pipe(dest(config.destDir));
